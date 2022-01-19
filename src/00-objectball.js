10e9dbedac21d92f54cf3c76dc6f9823cb0ff4e6
Kale Leach <kleach1212@gmail.com>
2022-01-18 18:47:53 -0800

    commit tree b96d4a1de87cc7c682fafd719664172184147ce3

---
+++ src/00-objectball.js
@@ -0,0 +1,64 @@
+function gameObject() {
+    'home': {
+        'teamName':'Brooklyn Nets',
+        'colors': ['Black', 'White']
+        'players': {
+            'Alan Anderson': {
+                Number: 0
+                Shoe: 16
+                Points: 22
+                Rebounds: 12
+                Assist: 
+                Steals:
+                Blocks:
+                Slam Dunks:
+            },
+            'Reggie Evans ': {
+                Number:
+                Shoe:
+                Points:
+                Rebounds:
+                Assist:
+                Steals:
+                Blocks:
+                Slam Dunks:
+            },
+            'Brook Lopez': {
+                Number:
+                Shoe:
+                Points:
+                Rebounds:
+                Assist:
+                Steals:
+                Blocks:
+                Slam Dunks:
+            },
+            'Mason Plumlee': {
+                Number:
+                Shoe:
+                Points:
+                Rebounds:
+                Assist:
+                Steals:
+                Blocks:
+                Slam Dunks:
+            },
+            Alan Anderson: {
+                Number:
+                Shoe:
+                Points:
+                Rebounds:
+                Assist:
+                Steals:
+                Blocks:
+                Slam Dunks:
+            }
+
+        },
+    },
+    away: {
+        teamName:'',
+        colors:'',
+        players:'',
+    }
+}
